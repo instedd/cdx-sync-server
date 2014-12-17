6232bf4b36d6a544294f14dd5f34f3c3f4bf991f
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2014-12-17 17:46:17 -0300

    Adding server stubs

---
+++ server/lib/rsync_file_watcher.rb
@@ -0,0 +1,51 @@
+require 'thread'
+module RSync
+  class FileWatcher
+
+    def initialize(sync_directory)
+      @jobs = Queue.new
+      @sync_directory = sync_directory
+      @watch_expression = "#{@sync_directory}/*/inbox/*"
+    end
+
+    def watch
+      enqueue_preexisting_files
+      start_monitoring
+      puts "Monitoring files in #{@sync_directory}..."
+      while true
+        next_file = @jobs.pop
+        yield next_file
+      end
+    end
+
+    def ensure_sync_directory
+      puts "Initializing RSync in #{@sync_directory}"
+      unless Dir.exists? @sync_directory
+        FileUtils.mkdir_p @sync_directory
+      end
+    end
+
+    private
+
+    def enqueue_preexisting_files
+      preexisting_files = Dir[@watch_expression]
+      if preexisting_files.any?
+        puts "Enqueuing #{preexisting_files.size} preexisting file(s)."
+        preexisting_files.each do |f|
+          @jobs << f
+        end
+      end
+    end
+
+    def start_monitoring
+      Thread.start do
+        FileWatcher.new(@sync_directory).watch() do |path, event|
+          if event == :new and File.fnmatch(@watch_expression, path)
+            puts "New file detected: #{path}."
+            @jobs << path
+          end
+        end
+      end
+    end
+  end
+end
